ba0e5908c6e7859b39d480d1e2310c865a3f8db7
CarlosCasadoo <carlos.casado@alumnos.uneatlantico.es>
2023-04-25 13:02:54 +0200

    feature: add client class and for loop to iterate data file

---
+++ entregas/carlosCasado/examen-parcial-p2/src/main/ex1/Client.java
@@ -0,0 +1,25 @@
+package main.ex1;
+
+public class Client {
+
+    private String name;
+    private String surname;
+    private String address;
+    private String phoneNumber;
+
+    public Client(String name, String surname, String address, String phoneNumber) {
+        this.name = name;
+        this.surname = surname;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFullName() {
+        return name + " " + surname;
+    }
+
+    public boolean equals(Client) {
+        //TO-DO
+    }
+
+}
