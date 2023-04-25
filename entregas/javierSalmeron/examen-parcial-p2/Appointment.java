0b329291bc9bb08aaf6b5a997e629e54ecb3bf53
0xJVR <114479588+0xJVR@users.noreply.github.com>
2023-04-25 13:25:39 +0200

    Add features to appointment, client and custom date

--- entregas/javierSalmeron/examen-parcial-p2/Appointment.java
+++ entregas/javierSalmeron/examen-parcial-p2/Appointment.java
@@ -6,4 +6,20 @@ public class Appointment {
     private CustomDate date;
     private String description;
 
+    public Appointment(Pet pet, CustomDate date, String description) {
+        this.pet = pet;
+        this.date = date;
+        this.description = description;
+    }
+
+    public String toString() {
+        return "[" + date.toString() + "] - " + 
+        owner.getFullName() + " - " + 
+        pet.getName() + " - " + 
+        description;
+    }
+
+    public void setOwner(Client owner) {
+        this.owner = owner;
+    }
 }
--- entregas/javierSalmeron/examen-parcial-p2/CustomDate.java
+++ entregas/javierSalmeron/examen-parcial-p2/CustomDate.java
@@ -4,5 +4,20 @@ public class CustomDate {
     private int day;
     private int month;
     private int year;
+    private String separator = "/";
 
+    public CustomDate(int day, int month, int year, String separator) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+        this.separator = separator;
+    }
+
+    public String toString() {
+        return day + separator + month + separator + year;
+    }
+
+    public String format(String format) {
+        return toString();
+    }
 }
