c941c1b0e2cd36db56a807f753c6dc1365800e4b
Sergio Moreno <113471709+srgiom@users.noreply.github.com>
2023-04-25 13:28:13 +0200

    Add files via upload

---
+++ entregas/morenoSergio/commit 2 - ex1/ManagementSystem.java
@@ -0,0 +1,40 @@
+package main.ex1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ManagementSystem {
+    private FileHandler fileHandler;
+
+    public ManagementSystem() {
+    }
+
+    public void loadData(String path) {
+        ReadFile = readFile = new ReadFile();
+        List<String> listClients;
+        try{
+            listClients = readFile.loadSource("esqueleto/data/clients.dat");
+
+            String name;
+            String surname;
+            String address;
+            String phoneNumber;
+
+
+            for(String line : listClients){
+                System.out.println(line);
+                String[] lineData=line.split(",");
+                String name = data[0].replaceAll("\"", "");
+                String surname = data[1].replaceAll("\"", "");
+                String address = data[2].replaceAll("\"", "");
+                String phoneNumber = data[3].replaceAll("\"", "");
+        } catch (FileNotFoundException e){
+            System.err.println(e.getMessage());
+        }
+    }
+    public void addAppointment(Appointment appointment, Client client) {
+       // Add appointment to list
+    }
+
+}
