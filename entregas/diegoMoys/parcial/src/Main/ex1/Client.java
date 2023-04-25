dae2d633853e2525b2dac3057702bbc32583f31a
Diego Moys <diegomoys01@gmail.com>
2023-04-25 14:00:43 +0200

    features(updating methods): updating methods of the solution

--- entregas/diegoMoys/parcial/src/Main/ex1/Client.java
+++ entregas/diegoMoys/parcial/src/Main/ex1/Client.java
@@ -17,7 +17,7 @@ public class Client {
         return this.name + " " + this.surname;
     }
 
-    public String compareEqualsClient(Client client) {
-        return this.getFullName();
+    boolean equals(Client client){
+        return this.name.equals(client.name) && this.surname.equals(client.surname);
     }
 }
