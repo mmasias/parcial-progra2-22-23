1f0670615e3b0308aaf4f896744d9ce9d5f85699
ana-deb <74409467+ana-deb@users.noreply.github.com>
2023-04-25 13:07:18 +0200

    Add files via upload

---
+++ examen-parcial-p2/src/main/ex1/CustomDate.java
@@ -0,0 +1,22 @@
+package main.ex1;
+
+public class CustomDate {
+
+    private int day;
+    private int month;
+    private int year;
+
+    public CustomDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public String toString() {
+        return null;
+    }
+
+    String format(String) { //porque falla tambien el puto parentesis
+        return null;
+    }
+}
