975283feba7fc930ff25021547833bd10076cfe5
josueMerino <josue.merino@alumnos.uneatlantico.es>
2023-04-25 14:10:39 +0200

    feature(test): Work done almost

--- entregas/josueMerino/examen-parcial-p2/src/main/ex1/CustomDate.java
+++ entregas/josueMerino/examen-parcial-p2/src/main/ex1/CustomDate.java
@@ -6,4 +6,23 @@ public class CustomDate {
     private int year;
 
 
+    public CustomDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return  day +
+                "/" + "0"+ month +
+                "/" + year ;
+    }
+
+    public String format(String format)
+    {
+        return  day +
+                format+ "0"+ month +
+                format+ year ;
+    }
 }
--- entregas/josueMerino/examen-parcial-p2/src/main/ex1/ManagementSystem.java
+++ entregas/josueMerino/examen-parcial-p2/src/main/ex1/ManagementSystem.java
@@ -7,9 +7,13 @@ import java.util.List;
 public class ManagementSystem {
     private FileHandler fileHandler;
 
+    private List<Client> clients;
 
+    private List<Appointment> appointments;
 
     public ManagementSystem() {
+        this.clients = new ArrayList<>();
+        this.appointments = new ArrayList<>();
     }
 
     public void loadData(String path) {
@@ -30,9 +34,43 @@ public class ManagementSystem {
             // Create client and add to list
 
          */
+
+        FileHandler file = new FileHandler();
+
+        List<String> lines = file.loadFileContent(path);
+
+        int i = 0;
+
+        for (String line: lines
+             ) {
+            String[] data = line.split(";");
+
+            if (i != 0){
+                // Extract data from line and remove quotes
+                String name = data[0].replaceAll("\"", "");
+                String surname = data[1].replaceAll("\"", "");
+                String address = data[2].replaceAll("\"", "");
+                String phoneNumber = data[3].replaceAll("\"", "");
+
+                Client client = new Client(name, surname, address, phoneNumber);
+
+                this.clients.add(client);
+            }
+            i++;
+        }
+
+
     }
     public void addAppointment(Appointment appointment, Client client) {
-       // Add appointment to list
+       this.appointments.add(appointment);
+       this.clients.add(client);
     }
 
+    public List<Client> getClients() {
+        return clients;
+    }
+
+    public List<Appointment> getAppointments() {
+        return appointments;
+    }
 }
