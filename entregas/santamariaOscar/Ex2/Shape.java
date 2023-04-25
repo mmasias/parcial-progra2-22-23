1b43ddf7f412f5abe782e961305bfa47f924d6c4
Oscar Santamaria <114479802+Oscarsantasanchez@users.noreply.github.com>
2023-04-25 13:29:02 +0200

    Create Shape.java

---
+++ entregas/santamariaOscar/Ex2/Shape.java
@@ -0,0 +1,28 @@
+public abstract class Shape {
+    private String color;
+    private boolean filled;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
