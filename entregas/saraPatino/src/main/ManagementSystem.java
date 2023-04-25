e74fa9723050889f0ce20e345b6109164a4fb221
sara-patino <sara.patino@alumnos.uneatlantico.com>
2023-04-25 13:55:19 +0200

    added the main file

--- entregas/saraPatino/src/main/ManagementSystem.java
+++ entregas/saraPatino/src/main/ManagementSystem.java
@@ -15,24 +15,26 @@ public class ManagementSystem {
     public void loadData(String path) {
         
         FileHandler fileHandlerExample = new FileHandler();
+
+        String name;
+        String surname;
+        String adress;
+        String phoneNumber;
         
         for (String client: fileHandlerExample.loadFileContent("src/main/data/clients.dat")) {
             String[] rawData;
             rawData = client.split(",");
 
-            String name;
             name = rawData[0].toString();
 
-            String surname;
             surname = rawData[1];
 
-            String adress;
             adress = rawData[2];
             
-            String phoneNumber;
             phoneNumber = rawData[3];
 
         }
+
 
     }
     public void addAppointment(Appointment appointment, Client client) {
