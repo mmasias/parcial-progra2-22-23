b48f5be2af62d168d3e37be7f41b0453e3de2d73
ceesargaarcia <cesar.garcia@alumnos.uneatlantico.es>
2023-04-25 12:45:46 +0200

    Started the exam

---
+++ entregas/garciaCesar/examen-parcial-p2/src/main/ex2/Shape.java
@@ -0,0 +1,23 @@
+package src.main.ex2;
+
+public class Shape {
+	String color;
+	boolean filled;
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public boolean isFilled() {
+		return filled;
+	}
+	public void setFilled(boolean filled) {
+		this.filled = filled;
+	}
+	public abstract double getArea() {
+
+	}
+	public abstract double getPerimeter();
+
+}
