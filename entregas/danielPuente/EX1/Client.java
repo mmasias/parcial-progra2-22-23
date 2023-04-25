d6e90916327c86eeef0262bb3ff7e19e8f51a34f
Dani Puente <114569957+Daaniii23@users.noreply.github.com>
2023-04-25 13:18:24 +0200

    Create Client.java

---
+++ entregas/danielPuente/EX1/Client.java
@@ -0,0 +1,43 @@
+public class Client {
+    String name;
+    String surname;
+    String adress;
+    String phoneNumber;
+
+    Client(String name, String surname, String adress, String phoneNumber) {
+
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+
+    }
+}
