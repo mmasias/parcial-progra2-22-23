0cd8b9bf19ec487e2a885209b39e37b978c0b839
Madeline Trejos <madeline.trejos@alumnos.uneatlantico.es>
2023-04-25 13:22:04 +0200

    create client

--- esqueleto/examen-parcial-p2/src/main/ex1/ManagementSystem.java
+++ esqueleto/examen-parcial-p2/src/main/ex1/ManagementSystem.java
@@ -13,23 +13,35 @@ public class ManagementSystem {
     }
 
     public void loadData(String path) {
-        /*
+    
             Load clients from file
+            List<String> lines = fileHandler.loadFileContent(path);
+
 
             // For each line in file
+            for (String line : lines) {
+              
+                if (line.startsWith("Name")) {
+                    continue;  
 
+            }
             String[] data = line.split(";");
 
-             // Extract data from line and remove quotes
+            Extract data from line and remove quotes
             String name = data[0].replaceAll("\"", "");
             String surname = data[1].replaceAll("\"", "");
             String address = data[2].replaceAll("\"", "");
             String phoneNumber = data[3].replaceAll("\"", "");
+   
 
-
+            
             // Create client and add to list
+            Client client = new Client(name, surname, address, phoneNumber);
+            this.clients.add(client);
 
-         */
+
+
+      
     }
     public void addAppointment(Appointment appointment, Client client) {
        // Add appointment to list
