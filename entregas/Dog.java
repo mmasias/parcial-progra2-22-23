a20a03b168090c7303cdb6b23df3076c13b34b55
BrendaLopes <66446283+BrendaLopes@users.noreply.github.com>
2023-04-25 13:55:34 +0200

    Add files via upload

---
+++ entregas/Dog.java
@@ -0,0 +1,11 @@
+package main.ex1;
+
+public class Dog extends Pet{
+    public Dog(String name, String color, int age) {
+        super(name, color, age);
+    }
+
+    public String getName() {
+        return name;
+    }
+}
