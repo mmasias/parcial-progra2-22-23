b036ace17699a72ffdc1895b9769f92878d6f1c9
Dievexx <diego30sscc@gmail.com>
2023-04-25 13:46:46 +0200

    added new methods in ex1

---
+++ entregas/garciaDiego/examen-parcial-p2/src/main/ex1/ManagementSystem.java
@@ -0,0 +1,46 @@
+package main.ex1;
+
+import java.util.List;
+
+public class ManagementSystem {
+    FileHandler fileHandler;
+    List<Client> clients;
+    List<Appointment> appointments;
+
+    public ManagementSystem() {
+        this.clients = new ArrayList<>();
+        this.appointments = new ArrayList<>();
+    }
+    void loadData(String path) {
+        List<String> lines = this.fileHandler.loadFileContent(path);
+        for (String line : lines) {
+            String[] data = line.split(",");
+            Client client = new Client(data[0], data[1], data[2]);
+            this.clients.add(client);
+        }
+    }
+
+    public List<Client> getClients() {
+        return clients;
+    }
+
+
+    void addAppointment(Appointment appointment, Client client) {
+        this.appointments.add(appointment);
+    }
+
+    public List<Appointment> getAppointments() {
+        return appointments;
+    }
+
+    private boolean isClient(Client client) {
+        for (Appointment appointment : this.appointments) {
+            if (appointment.getClient().equals(client)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+}
