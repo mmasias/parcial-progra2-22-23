618428ef9ab11b9ee02d47ff061023b0484cfdb0
joserasilla-uneat <jose.rasilla@alumnos.uneatlantico.es>
2023-04-25 13:54:54 +0200

    FIX

--- entregas/joseRasilla/examen-parcial-p2/src/main/Cat.java
+++
@@ -1,7 +0,0 @@
-public class Cat extends Pet{
-    public Cat(String name, String color, int age) {
-        this.name = name;
-        this.color = color;
-        this.age = age;
-    }
-}
--- entregas/joseRasilla/examen-parcial-p2/src/main/Dog.java
+++
@@ -1,7 +0,0 @@
-public class Dog extends Pet{
-    public Dog(String name, String color, int age) {
-        this.name = name;
-        this.color = color;
-        this.age = age;
-    }
-}
