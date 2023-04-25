5c231f7ddd93a6ed5a985f3984b89f5a425e5e72
Pablo <pablo.arce1@alumnos.uneatlantico.es>
2023-04-25 11:12:51 +0200

    Not given classes are set, excluding a method from Client and the given classes FileHandler and ManagementSystem

--- entregas/pabloArce/src/main/ex1/Appointment.java
+++ entregas/pabloArce/src/main/ex1/Appointment.java
@@ -1,5 +1,19 @@
 package main.ex1;
 
 public class Appointment {
-
+    private Client owner;
+    private Pet pet;
+    private CustomDate date;
+    private String description;
+    public Appointment(Pet pet, CustomDate date, String description) {
+        this.pet = pet;
+        this.date = date;
+        this.description = description;
+    }
+    public String toString() {
+        return "Appointment [owner=" + owner + ", pet=" + pet + ", date=" + date + ", description=" + description + "]";
+    }
+    public void setOwner(Client owner) {
+        this.owner = owner;
+    }
 }
