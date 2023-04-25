74d737443f2b5e5f4347268fc0e2dc656ab201e5
luciaZama <lzamanillol@gmail.com>
2023-04-25 13:35:50 +0200

    feature: finished the class Appointment

--- entregas/luciaZamanillo/ejercicio1/src/main/Appointment.java
+++ entregas/luciaZamanillo/ejercicio1/src/main/Appointment.java
@@ -2,5 +2,26 @@ package main;
 
 public class Appointment {
     Client owner;
+    Pet pet;
+    CustomDate date;
+    String description;
 
+    public Appointment(Pet pet, CustomDate date, String description) {
+        this.pet = pet;
+        this.date = date;
+        this.description = description;
+    }
+
+    public String toString() {
+        String fullDate = date.toString();
+        String fullName = owner.getFullName();
+        String petName = pet.getName();
+
+        String fullInfo = "[" + fullDate + "]" + " - " + fullName + " - " + petName + " - " + this.description;
+        return fullInfo;
+    }
+
+    public void setOwner(Client owner) {
+        this.owner = owner;
+    }
 }
