da5de9702e0b6e22af647e319297181be13d36d5
FernandoWilliam26 <fernando.calvo@alumnos.uneatlantico.es>
2023-04-25 14:17:30 +0200

    Este es mi Commit del Examen Parcial de Programación II

---
+++ entregas/fernandoCalvo/parcial-progra2-22-23/src/EX1/CustomDate.java
@@ -0,0 +1,55 @@
+package EX1;
+
+public class CustomDate {
+
+    private int day;
+    private int month;
+    private int year;
+
+    public CustomDate(int day,int month,int year){
+
+        this.day = day;
+        this.month = month;
+        this.year = year;
+
+    }
+
+    @Override
+    public String toString() {
+        return "CustomDate{" +
+                "day=" + day +
+                ", month=" + month +
+                ", year=" + year +
+                '}';
+    }
+
+    public String format(String format){
+
+        return format;
+
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
---
+++ entregas/fernandoCalvo/parcial-progra2-22-23/src/EX1/ManagementSystem.java
@@ -0,0 +1,51 @@
+package EX1;
+
+import java.util.List;
+import java.util.logging.FileHandler;
+
+public class ManagementSystem {
+
+    private FileHandler fileHandler;
+    private List<Client> clients;
+    private List<Appointment> appointments;
+
+    public ManagementSystem(){
+
+
+
+    }
+
+    public void loadData(String d){
+
+
+
+
+    }
+
+    public List<Client> getClients(){
+
+
+        return clients;
+
+    }
+
+    public void addAppointment(Appointment app, Client client){
+
+
+
+    }
+
+    public List<Appointment> getAppointments(){
+
+        return appointments;
+
+    }
+
+    private Client isClient(Client client){
+
+        return client;
+
+    }
+
+
+}
